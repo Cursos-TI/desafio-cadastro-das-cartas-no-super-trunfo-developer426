9f17c698629971acc09bd5229b14cc7f3b238241
developer426 <matheeusnevess@gmail.com>
2025-08-27 20:23:59 +0000

    Cadastro carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -19,6 +19,31 @@ int main(){
     float pib2;
     int num_pontos_turisticos2;
 
+    printf ("--- Cadastro de Carta 1 ---\n");
+    
+    printf("Digite o estado (A-H): ");
+    scanf(" %c", &estado1); // O espaço que vem antes de '%c' ignora os espaços em branco e quebra de linha
+
+    printf("Digite o codigo da carta (Ex: A01): ");
+    scanf("%s", codigo_carta1);
+
+    printf("Digite o nome da cidade: ");
+    scanf(" %[^\n]", nome_cidade1); // Realiza a linha inteira
+
+    printf("Digite a populacao: ");
+    scanf("%d", &populacao1);
+
+    printf("Digite a area (em km2): ");
+    scanf("%f", &area1);
+
+    printf("Digite o PIB: ");
+    scanf("%f", &pib1);
+
+    printf("Digite o numero de pontos turisticos: ");
+    scanf("%d", &num_pontos_turisticos1);
+
+
+
 
 
 
